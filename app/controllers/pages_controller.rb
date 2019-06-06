e0a5ee9136f5f246847ce2c0ff10235befda1ac4
Ghislain Chavanne <gh.chavanne@gmail.com>
2019-06-06 16:29:51 +0200

    refont

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,4 @@ class PagesController < ApplicationController
   def home
   end
 
-  def shrimpfishery
-  end
-
-  def surfantastic
-  end
-
-  def ohmeohmy
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'shrimpfishery',       to: 'pages#shrimpfishery'
-  get 'surfantastic',       to: 'pages#surfantastic'
-  get 'ohmeohmy',       to: 'pages#ohmeohmy'
 end
